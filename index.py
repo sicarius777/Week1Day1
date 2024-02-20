1dd3b7b3831724586ad7f11ec206ab70536d9997
cameronmcnz <you@example.com>
2024-02-19 20:53:26 -0500

    Day1

---
+++ index.py
@@ -0,0 +1,26 @@
+# print("welcome")
+# print("welcome to CT Rhett!")
+# name = 'Rhett'
+# print("welcome to CT" + name)
+
+# print(f"welcome to CT {name}")
+
+
+def namer(f_name):
+    print(f"WELCOME WELCOME WELCOME CT {f_name}")
+    print("this will run too!")
+
+# namer("Jenequa")
+# namer("Zack")
+# namer("Rhett")
+# namer("Harrison")
+# print(namer("Adrian") + namer("Bryce"))
+print(namer("Adrian"))
+# print(namer("Adrian") == 3)
+# name2 = "Adrian"
+
+
+# def adder(num1, num2):
+#     return num1 + num2
+
+# print(adder(5, 6))
